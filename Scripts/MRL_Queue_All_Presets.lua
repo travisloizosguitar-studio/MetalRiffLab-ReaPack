545560c64d3819a469b9a119c88dea544713cbc7
travisloizosguitar-studio <travisloizosguitar@gmail.com>
2026-01-18 21:09:40 -0700

    Save local changes before rebase

--- Scripts/MRL_Queue_All_Presets.lua
+++ Scripts/MRL_Queue_All_Presets.lua
@@ -2,3 +2,33 @@
 -- v1.0.0
 -- Author: Travis Loizos
 reaper.ShowMessageBox("MRL Queue All Presets loaded successfully!", "Metal Riff Lab", 0)
+
+
+
+=============
+
+-- Metal Riff Lab : Render Mode GUI
+-- v1.0.0
+-- Author: Travis Loizos
+reaper.ShowMessageBox("MRL Render Mode GUI loaded successfully!", "Metal Riff Lab", 0)
+
+=============
+
+Copy code:
+
+https://raw.githubusercontent.com/travisloizosguitar-studio/MetalRiffLab-ReaPack/refs/heads/main/Scripts/MRL_Render_Mode_GUI.lua
+===========
+<source>https://raw.githubusercontent.com/travisloizosguitar-studio/MetalRiffLab-ReaPack/main/Scripts/MRL_Render_Mode_GUI.lua</source>
+=========
+<source>https://github.com/travisloizosguitar-studio/MetalRiffLab-ReaPack/raw/main/Scripts/MRL_Render_Mode_GUI.lua</source>
+
+====
+
+https://raw.githubusercontent.com/travisloizosguitar-studio/MetalRiffLab-ReaPack/main/Scripts/MRL_Queue_All_Presets.lua
+
+https://raw.githubusercontent.com/travisloizosguitar-studio/MetalRiffLab-ReaPack/main/Scripts/MRL_Queue_All_Presets.lua
+
+===
+https://raw.githubusercontent.com/travisloizosguitar-studio/MetalRiffLab-ReaPack/refs/heads/main/Scripts/MRL_Render_Mode_GUI.lua
+
+
